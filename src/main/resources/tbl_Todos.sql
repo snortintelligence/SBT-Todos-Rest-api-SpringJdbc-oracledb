60b8f12c17e85a73cff9dec5f3a0bc60cef77571
snortintelligence <snortintelligence@gmail.com>
2022-08-30 23:29:25 +0530

    modified sql file

--- src/main/resources/tbl_Todos.sql
+++ src/main/resources/tbl_Todos.sql
@@ -6,8 +6,3 @@ completed  varchar2(20),
 assigned_Date date,
 updated_Date date
 );
-
----------------------
-insert into tbl_Todos(TASK_ID,TITLE,DESCRIPTION,COMPLETED,ASSIGNED_DATE) values(101,'Create rest api','Todos rest api','COMPLETED',sysdate);
-
-update tbl_todos set TITLE='Create rest api.',DESCRIPTION='Create rest api with db.',COMPLETED='PENDING', UPDATED_DATE=sysdate where TASK_ID=101;
